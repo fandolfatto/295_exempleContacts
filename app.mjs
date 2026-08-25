448caa4da9ddf401b1e1d678594b2e2f7412475e
Frederique Andolfatto <pk37fki@eduvaud.ch>
2026-08-25 16:08:58 +0200

    add some comments

--- app.mjs
+++ app.mjs
@@ -1,7 +1,7 @@
 //On importe le module express (il doit être installé avec "npm install express")
 import express from 'express';
 import contacts from "./db/mock-contacts.mjs";
-// On crée une application Express
+// On crée une application Express qui est donnée au serveur HTTP pour qu'elle puisse traiter les requêtes reçues
 const app = express();
 // On définit le port sur lequel le serveur va écouter
 const port = process.env.PORT || 3000;
@@ -22,7 +22,11 @@ app.get("/:id", (req, res) => {
 });
 
 
-// On démarre le serveur et on lui dit d’écouter sur le port défini
+// On crée et démarre le serveur et on lui dit d’écouter sur le port défini
+// Le serveur ransmettra ensuite les requêtes à l'application Express qui cherchera comment y répondre grâce à ses routes.
+//On fait quelque chose de ce style en fait :
+//const server = http.createServer(app);
+//server.listen(3000);
 // Une fois lancé, le message est affiché dans la console
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
