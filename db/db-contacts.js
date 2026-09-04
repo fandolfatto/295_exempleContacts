9283fc95c6e1d90c0a7d8e8387ddc4d827b4554f
Frederique Andolfatto <pk37fki@eduvaud.ch>
2026-09-04 13:30:09 +0200

    use a connection pool and add package.json

--- db/db-contacts.js
+++ db/db-contacts.js
@@ -2,9 +2,10 @@ import mysql from 'mysql2/promise';
 
 // represents the connection to the DB
 // it is shared by all the fonctions
+// we will find another more secure solution in the next step
 const poolConn = mysql.createPool({
     host: "localhost",
-    user: "root", //use another user
+    user: "root", //use another user than root
     password: "P@ssw0rd",
     port: 3308, //usually we use the 3306 port
     database: "app_contacts",
@@ -17,6 +18,10 @@ const db = {
         // await waits for the query to finish before continuing
         // this function, but does not block the Node.js server
         const [rows] = await poolConn.execute('SELECT * FROM contacts');
+        // equivalent to
+        // const result = await poolConn.execute('SELECT * FROM contacts');
+        // const rows = result[0];
+        // information about the fields is the second part of the result
         return rows;
     },
 
